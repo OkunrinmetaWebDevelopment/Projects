23be2732bc33d6179d3821cacebf9b191e1e7c38
Bobby <bobimayaram@gmail.com>
2018-08-15 17:34:45 +0400

    Adding Boostrap
    Added a nav-bar for the admin page and modified the Login page

--- School-Management-System-With-PHP-master/module/admin/index.php
+++ School-Management-System-With-PHP-master/module/admin/index.php
@@ -3,34 +3,58 @@ include_once('main.php');
 ?>
 <html>
     <head>
-		    <link rel="stylesheet" type="text/css" href="../../source/CSS/style.css">
-				<script src = "JS/login_logout.js"></script>
-		</head>
+          <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    
+        <link rel="stylesheet" type="text/css" href="../../source/CSS/style.css">
+		<script src = "JS/login_logout.js"></script>
+	</head>
     <body>
-			  <div class="header"><h1>School Management System</h1></div>
-			  <div class="divtopcorner">
-				    <img src="../../source/logo.jpg" height="150" width="150" alt="School Management System"/>
-				</div>
-			<br/><br/>
-				<ul>
-				    <li class="manulist">
-						    <a class ="menulista" href="index.php">Home</a>
-								<a class ="menulista" href="manageStudent.php">Manage Student</a>
-								<a class ="menulista" href="manageTeacher.php">Manage Teacher</a>
-								<a class ="menulista" href="manageParent.php">Manage Parent</a>
-								<a class ="menulista" href="manageStaff.php">Manage Staff</a>
-								<a class ="menulista" href="course.php">Course</a>
-								<a class ="menulista" href="attendance.php">Attendance</a>
-								<a class ="menulista" href="examSchedule.php">Exam Schedule</a>
-								<a class ="menulista" href="salary.php">Salary</a>
-								<a class ="menulista" href="report.php">Report</a>
-								<a class ="menulista" href="payment.php">Payment</a>
-								<div align="center">
-								<h4>Hi!admin <?php echo $check." ";?></h4>
-								    <a class ="menulista" href="logout.php" onmouseover="changemouseover(this);" onmouseout="changemouseout(this,'<?php echo ucfirst($loged_user_name);?>');"><?php echo "Logout";?></a>
-						    </div>
-						</li>
-				</ul>
-			  <hr/>
-		</body>
+        
+        <nav class="navbar navbar-inverse">
+		  	<div class="container-fluid">
+		    	<div class="navbar-header">
+		      		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span>                      
+		      		</button>
+		      		<a class="navbar-brand" href="index.php">School Management System</a>
+		    	</div>
+			    <div class="collapse navbar-collapse" id="myNavbar">
+			      	<ul class="nav navbar-nav">
+				        <li class="active"><a href="index.php">Home</a></li>
+				        <li class="dropdown">
+			          		<a class="dropdown-toggle" data-toggle="dropdown" href="#">Management<span class="caret"></span></a>
+			          		<ul class="dropdown-menu">
+					            <li><a href="manageStudent.php">Manage Student</a></li>
+					            <li><a href="manageTeacher.php">Manage Teacher</a></li>
+					            <li><a href="manageParent.php">Manage Parent</a></li>
+					            <li><a href="manageStaff.php">Manage Staff</a></li>
+			          		</ul>
+			        	</li>
+			       		<li><a href="course.php">Course</a></li>
+			        	<li><a href="attendance.php">Attendance</a></li>
+			        	<li><a href="examSchedule.php">Exam Schedule</a></li>
+			        	<li><a href="salary.php">Salary</a></li>
+			        	<li><a href="report.php">Report</a></li>
+			        	<li><a href="payment.php">Payment</a></li>
+			      	</ul>
+			      	<ul class="nav navbar-nav navbar-right">
+			        	 	<li><a href="logout.php" onmouseover="changemouseover(this);" onmouseout="changemouseout(this,'<?php echo ucfirst($loged_user_name);?>');"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
+			      	</ul>
+			    </div>
+		  	</div>
+		</nav>
+		<br/><br/>
+		<div class="container">
+			<div class="header"><h1>School Management System</h1></div>
+			<div class="divtopcorner">
+				<img src="../../source/logo.jpg" height="150" width="150" alt="School Management System"/>
+			</div>
+		</div>
+	</body>
 </html>
